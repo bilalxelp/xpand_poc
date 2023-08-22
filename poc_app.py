9c1e114114cfdf2cb60b400e960c1882f14a0cee
Bilal Afzal <106112000+bilalxelp@users.noreply.github.com>
2023-08-22 11:23:54 +0530

    Update poc_app.py
    Added submit button

--- poc_app.py
+++ poc_app.py
@@ -262,26 +262,25 @@ def process_data(df, column_name, tab_name):
     
     return df
 
-# Define the Streamlit app
+
 def main():
     # Set the page title and icon
     # st.set_page_config(page_title='Data Processor', page_icon=':pencil:')
     st.set_page_config(page_title='My Streamlit App', page_icon=':bar_chart:', layout='wide', initial_sidebar_state='auto')
 
-
     # Set the sidebar options
     options = ['Viewpoint Classifier', 'Stance Feminist', 'Toxicity Detection', 'Personality Trait', 'Communication Style', 'Big-5 Personality']
     option_selected = st.sidebar.selectbox("Select a model:", options)
     
-
-    # Add more options as needed
-
     # Set the sidebar inputs
     uploaded_file = st.file_uploader("Upload a CSV or Excel file")
     column_name = st.text_input("Enter the column name to process")
+    
+    # Add a "Submit" button
+    submit_button = st.button("Submit")
 
-    # Check if file was uploaded and column name is provided
-    if uploaded_file is not None and column_name != "":
+    # Check if file was uploaded and column name is provided, and if the "Submit" button is clicked
+    if submit_button and uploaded_file is not None and column_name != "":
         # Read the file into a DataFrame
         if uploaded_file.name.endswith('csv'):
             df = pd.read_csv(uploaded_file)
@@ -310,3 +309,59 @@ def main():
 # Run the app
 if __name__ == '__main__':
     main()
+
+
+
+
+
+
+
+
+# # Define the Streamlit app
+# def main():
+#     # Set the page title and icon
+#     # st.set_page_config(page_title='Data Processor', page_icon=':pencil:')
+#     st.set_page_config(page_title='My Streamlit App', page_icon=':bar_chart:', layout='wide', initial_sidebar_state='auto')
+
+
+#     # Set the sidebar options
+#     options = ['Viewpoint Classifier', 'Stance Feminist', 'Toxicity Detection', 'Personality Trait', 'Communication Style', 'Big-5 Personality']
+#     option_selected = st.sidebar.selectbox("Select a model:", options)
+    
+
+#     # Add more options as needed
+
+#     # Set the sidebar inputs
+#     uploaded_file = st.file_uploader("Upload a CSV or Excel file")
+#     column_name = st.text_input("Enter the column name to process")
+
+#     # Check if file was uploaded and column name is provided
+#     if uploaded_file is not None and column_name != "":
+#         # Read the file into a DataFrame
+#         if uploaded_file.name.endswith('csv'):
+#             df = pd.read_csv(uploaded_file)
+#         elif uploaded_file.name.endswith('xls') or uploaded_file.name.endswith('xlsx'):
+#             df = pd.read_excel(uploaded_file)
+#         else:
+#             st.error("Invalid file type. Please upload a CSV or Excel file.")
+
+#         # Check if the selected column exists in the DataFrame
+#         if column_name not in df.columns:
+#             st.error(f"The column '{column_name}' does not exist in the uploaded file.")
+#         else:
+#             # Process the data based on the selected option
+#             processed_data = process_data(df, column_name, option_selected)
+
+#             # Display the processed data
+#             st.write("Processed Data:")
+#             st.write(processed_data)
+
+#             # Allow the user to download the processed data as a CSV file
+#             csv = processed_data.to_csv(index=False)
+#             b64 = base64.b64encode(csv.encode()).decode()
+#             href = f'<a href="data:file/csv;base64,{b64}" download="processed_data.csv">Download processed data</a>'
+#             st.markdown(href, unsafe_allow_html=True)
+
+# # Run the app
+# if __name__ == '__main__':
+#     main()
