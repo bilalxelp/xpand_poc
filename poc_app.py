065276bac394dcb386c77d831dce71c3221416bf
Bilal Afzal <106112000+bilalxelp@users.noreply.github.com>
2023-08-22 13:56:32 +0530

    Update poc_app.py

--- poc_app.py
+++ poc_app.py
@@ -10,7 +10,6 @@ import requests
 import ast
 
 
-
 def remove_non_english(df, column_name):
     # Create a new column with language codes for each row in the specified column
     df['lang'] = ''
@@ -49,49 +48,48 @@ def stanza_tokenizer(text):
         
     return clean_sent
 
+def Viewpoint_classifier(df, column_name):
+  
+  classifier = pipeline("text-classification", model="lighteternal/fact-or-opinion-xlmr-el", tokenizer="lighteternal/fact-or-opinion-xlmr-el")
 
+  df['Viewpoint'] = ''
 
-def viewpoint_classifier(df, column_name):
-    
-    viewpoint_classifier_model = pipeline("text-classification", model="lighteternal/fact-or-opinion-xlmr-el", tokenizer="lighteternal/fact-or-opinion-xlmr-el")
-    
-	df['Viewpoint'] = ''
-    
-    for x in range(len(df)):
-		result = viewpoint_classifier_model(str(df[column_name][x]))
-        if result[0]['label'] == 'LABEL_0':
-			df['Viewpoint'][x] = "Opinion"
+  for x in range(len(df)):
+    result = classifier(str(df[column_name][x]))
+    if result[0]['label'] == 'LABEL_0':
+      df['Viewpoint'][x] = "Opinion"
     elif result[0]['label'] == 'LABEL_1':
-		df['Viewpoint'][x] = "Fact"
-    
-      return df
+      df['Viewpoint'][x] = "Fact"
+
+  return df
 
 def stance_feminist(df, column_name):
-    
-	stance_feminist_classifier_model = pipeline("text-classification", model="cardiffnlp/twitter-roberta-base-stance-feminist", tokenizer="cardiffnlp/twitter-roberta-base-stance-feminist")
-	
-	df['Label'] = ''
-	df['Score'] = ''
-	for x in range(len(df)):
-		result = stance_feminist_classifier_model(df[column_name][x])
-		df['Label'][x] = result[0]['label']
-		df['Score'][x] = result[0]['score']
-		
-	return df
 
-def toxic_Detection(df, column_name):
-    
-    toxicity_classifier_model = pipeline("text-classification", model="martin-ha/toxic-comment-model", tokenizer="martin-ha/toxic-comment-model")
-	
-	df['Label'] = ''
-	df['Score'] = ''
+  classifier = pipeline("text-classification", model="cardiffnlp/twitter-roberta-base-stance-feminist", tokenizer="cardiffnlp/twitter-roberta-base-stance-feminist")
 
-	for x in range(len(df)):
-    	result = toxicity_classifier_model(df[column_name][x])
-    	df['Label'][x] = result[0]['label']
-    	df['Score'][x] = result[0]['score']
+  df['Label'] = ''
+  df['Score'] = ''
 
-	return df
+  for x in range(len(df)):
+    result = classifier(df[column_name][x])
+    df['Label'][x] = result[0]['label']
+    df['Score'][x] = result[0]['score']
+
+  return df
+
+def Toxic_Detection(df, column_name):
+
+  classifier = pipeline("text-classification", model="martin-ha/toxic-comment-model", tokenizer="martin-ha/toxic-comment-model")
+
+  df['Label'] = ''
+  df['Score'] = ''
+
+  for x in range(len(df)):
+    result = classifier(df[column_name][x])
+    df['Label'][x] = result[0]['label']
+    df['Score'][x] = result[0]['score']
+
+  return df
 
 def personality_trait(df, column_name):
     payload = []
@@ -227,51 +225,44 @@ def big_five_personality(df, column_name):
 # Function to process the selected tab
 def process_data(df, column_name, tab_name):
     
-    df_copy = df.copy()  # Create a copy of the original DataFrame
+    df.reset_index(inplace=True, drop=True)
     
-    df_copy.reset_index(inplace=True, drop=True)
-    
-    try:
-        if tab_name == 'Viewpoint Classifier':
-          df_copy = remove_non_english(df_copy, column_name)
-          df_copy['Split Sentences'] = df_copy[column_name].apply(lambda x: stanza_tokenizer(x))
-          df_copy = df_copy.explode("Split Sentences").reset_index(drop=True)
-          df_copy = viewpoint_classifier(df_copy, "Split Sentences")
-    
-        elif tab_name == 'Stance Feminist':
-          df_copy = remove_non_english(df_copy, column_name)
-          df_copy['Split Sentences'] = df_copy[column_name].apply(lambda x: stanza_tokenizer(x))
-          df_copy = df_copy.explode("Split Sentences").reset_index(drop=True)
-          df_copy = stance_feminist(df_copy, "Split Sentences")
-    
-        elif tab_name == 'Toxicity Detection':
-          df_copy = remove_non_english(df_copy, column_name)
-          df_copy['Split Sentences'] = df_copy[column_name].apply(lambda x: stanza_tokenizer(x))
-          df_copy = df_copy.explode("Split Sentences").reset_index(drop=True)
-          df_copy = toxic_Detection(df_copy, "Split Sentences")
-    
-        elif tab_name == 'Personality Trait':
-          df_copy = remove_non_english(df_copy, column_name)
-          df_copy = personality_trait(df_copy, column_name)
-    
-        elif tab_name == 'Communication Style':
-          df_copy = remove_non_english(df_copy, column_name)
-          df_copy = communication_style(df_copy, column_name)
-    
-        elif tab_name == 'Big-5 Personality':
-          df_copy = remove_non_english(df_copy, column_name)
-          df_copy = big_five_personality(df_copy, column_name)
+    if tab_name == 'Viewpoint Classifier':
+      df = remove_non_english(df, column_name)
+      df['Split Sentences'] = df[column_name].apply(lambda x: stanza_tokenizer(x))
+      df = df.explode("Split Sentences").reset_index(drop=True)
+      df = Viewpoint_classifier(df, "Split Sentences")
 
-    except Exception as e:
-        st.error(f"An error occurred while processing the data: {e}")
-        df_copy = pd.DataFrame()
+    elif tab_name == 'Stance Feminist':
+      df = remove_non_english(df, column_name)
+      df['Split Sentences'] = df[column_name].apply(lambda x: stanza_tokenizer(x))
+      df = df.explode("Split Sentences").reset_index(drop=True)
+      df = stance_feminist(df, "Split Sentences")
+
+    elif tab_name == 'Toxicity Detection':
+      df = remove_non_english(df, column_name)
+      df['Split Sentences'] = df[column_name].apply(lambda x: stanza_tokenizer(x))
+      df = df.explode("Split Sentences").reset_index(drop=True)
+      df = Toxic_Detection(df, "Split Sentences")
+
+    elif tab_name == 'Personality Trait':
+      df = remove_non_english(df, column_name)
+      df = personality_trait(df, column_name)
+
+    elif tab_name == 'Communication Style':
+      df = remove_non_english(df, column_name)
+      df = communication_style(df, column_name)
+
+    elif tab_name == 'Big-5 Personality':
+      df = remove_non_english(df, column_name)
+      df = big_five_personality(df, column_name)
 
     # Add more options as needed
     
-    return df_copy  # Return the processed copy of the DataFrame
+    return df
 
 
-
+# Define the Streamlit app
 def main():
     # Set the page title and icon
     # st.set_page_config(page_title='Data Processor', page_icon=':pencil:')
@@ -318,59 +309,3 @@ def main():
 # Run the app
 if __name__ == '__main__':
     main()
-
-
-
-
-
-
-
-
-# # Define the Streamlit app
-# def main():
-#     # Set the page title and icon
-#     # st.set_page_config(page_title='Data Processor', page_icon=':pencil:')
-#     st.set_page_config(page_title='My Streamlit App', page_icon=':bar_chart:', layout='wide', initial_sidebar_state='auto')
-
-
-#     # Set the sidebar options
-#     options = ['Viewpoint Classifier', 'Stance Feminist', 'Toxicity Detection', 'Personality Trait', 'Communication Style', 'Big-5 Personality']
-#     option_selected = st.sidebar.selectbox("Select a model:", options)
-    
-
-#     # Add more options as needed
-
-#     # Set the sidebar inputs
-#     uploaded_file = st.file_uploader("Upload a CSV or Excel file")
-#     column_name = st.text_input("Enter the column name to process")
-
-#     # Check if file was uploaded and column name is provided
-#     if uploaded_file is not None and column_name != "":
-#         # Read the file into a DataFrame
-#         if uploaded_file.name.endswith('csv'):
-#             df = pd.read_csv(uploaded_file)
-#         elif uploaded_file.name.endswith('xls') or uploaded_file.name.endswith('xlsx'):
-#             df = pd.read_excel(uploaded_file)
-#         else:
-#             st.error("Invalid file type. Please upload a CSV or Excel file.")
-
-#         # Check if the selected column exists in the DataFrame
-#         if column_name not in df.columns:
-#             st.error(f"The column '{column_name}' does not exist in the uploaded file.")
-#         else:
-#             # Process the data based on the selected option
-#             processed_data = process_data(df, column_name, option_selected)
-
-#             # Display the processed data
-#             st.write("Processed Data:")
-#             st.write(processed_data)
-
-#             # Allow the user to download the processed data as a CSV file
-#             csv = processed_data.to_csv(index=False)
-#             b64 = base64.b64encode(csv.encode()).decode()
-#             href = f'<a href="data:file/csv;base64,{b64}" download="processed_data.csv">Download processed data</a>'
-#             st.markdown(href, unsafe_allow_html=True)
-
-# # Run the app
-# if __name__ == '__main__':
-#     main()
